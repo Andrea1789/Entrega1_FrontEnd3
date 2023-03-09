f4f2b8d52ce40f432921c038318ba1c3c5998bf2
Andrea1789 <andreaagudelocardona789@gmail.com>
2023-03-08 22:18:59 -0500

    Styles

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Card.css';
 
 const Card = ({nombre, generoMusical}) => {
   return (
-    <div>
+    <div className='card'>
       <h3>{nombre}</h3>
       <h3>{generoMusical}</h3>
    </div>
